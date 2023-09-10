dcf87fabb4a632005ff81e3b46685856cfd91a00
funwithme2021 <125599198+funwithme2021@users.noreply.github.com>
2023-09-10 11:17:40 +0800

    Add files via upload

--- 43rd中原會計系系學會檔案上傳與查詢上傳系統/announcement.js
+++ 43rd中原會計系系學會檔案上傳與查詢上傳系統/announcement.js
@@ -1,8 +1,8 @@
 document.getElementById("itemsPerPageSelect").addEventListener("change", () => {
-    itemsPerPage = parseInt(document.getElementById("itemsPerPageSelect").value);
-    currentPage = 1;
-    updateAnnouncementTable();
-    updatePagination();
+  itemsPerPage = parseInt(document.getElementById("itemsPerPageSelect").value);
+  currentPage = 1;
+  updateAnnouncementTable();
+  updatePagination();
 });
 const announcements = [
 {
@@ -11,10 +11,15 @@ title: "每日13-14時及每週六0-6時為維護時段，暫停使用。",
 note: "網頁維護公告"
 },
 {
-  date: "2023-09-09",
-  title: "自112年9月9日起，簡易版本網頁內檔案查詢系統停止運作，請使用電腦版本網頁操作。",
-  note: "網頁停用公告"
-  },
+date: "2023-09---",
+title: "112年9月-日-時-分，於中原大學管理大樓401室辦理1121學年度9月份系代會。",
+note: "<a href='https://funwithme.github.io/'>開會通知公告</a>"
+},
+{
+date: "2023-09-09",
+title: "自112年9月9日起，簡易版本網頁停止運作，請使用電腦版本網頁操作。",
+note: "網頁停止公告"
+},
 {
 date: "2023-08-10",
 title: "自112年8月10日起，簡易版本網頁內檔案查詢系統停止更新，請使用電腦版本網頁操作。",
@@ -28,7 +33,7 @@ note: "<a href='https://funwithme2021.github.io/cyac43.github.io/43rd%E4%B8%AD%E
 {
 date: "2023-07-20",
 title: "簡易版本網頁",
-note: "<a href='https://funwithme2021.github.io/43rdac.github.io/43rd%E4%B8%AD%E5%8E%9F%E6%9C%83%E8%A8%88%E7%B3%BB%E7%B3%BB%E5%AD%B8%E6%9C%83%E6%AA%94%E6%A1%88%E4%B8%8A%E5%82%B3%E8%88%87%E6%9F%A5%E8%A9%A2%E4%B8%8A%E5%82%B3%E7%B3%BB%E7%B5%B1/index.html?fbclid=IwAR0FnADHDKRIGK2IvE-0p7Yri8e7WUJvBoaqs2dGFdnWpgcSBU2annykUyw'>點擊進入系統</a>"
+note: "<a href='https://unwithme2021.github.io/43rdac.github.io/43rd%E4%B8%AD%E5%8E%9F%E6%9C%83%E8%A8%88%E7%B3%BB%E7%B3%BB%E5%AD%B8%E6%9C%83%E6%AA%94%E6%A1%88%E4%B8%8A%E5%82%B3%E8%88%87%E6%9F%A5%E8%A9%A2%E4%B8%8A%E5%82%B3%E7%B3%BB%E7%B5%B1/index.html?fbclid=IwAR0FnADHDKRIGK2IvE-0p7Yri8e7WUJvBoaqs2dGFdnWpgcSBU2annykUyw'>點擊進入系統</a>"
 },
 {
 date: "2023-07-15",
@@ -55,28 +60,28 @@ const startIndex = (currentPage - 1) * itemsPerPage;
 const endIndex = startIndex + itemsPerPage;
 const currentPageAnnouncements = filteredAnnouncements.slice(startIndex, endIndex);
 
-  table.innerHTML = `
-    <thead>
+table.innerHTML = `
+  <thead>
+    <tr>
+      <th>公告日期</th>
+      <th>公告事項</th>
+      <th>備註</th>
+    </tr>
+  </thead>
+  <tbody>
+    ${currentPageAnnouncements.map((announcement) => `
       <tr>
-        <th>公告日期</th>
-        <th>公告事項</th>
-        <th>備註</th>
+        <td>${announcement.date}</td>
+        <td>${announcement.title}</td>
+        <td>${announcement.note}</td>
       </tr>
-    </thead>
-    <tbody>
-      ${currentPageAnnouncements.map((announcement) => `
-        <tr>
-          <td>${announcement.date}</td>
-          <td>${announcement.title}</td>
-          <td>${announcement.note}</td>
-        </tr>
-      `).join("")}
-    </tbody>
-  `;
+    `).join("")}
+  </tbody>
+`;
 }
 
 function updatePagination() {
-    const prevPageBtn = document.getElementById("prevPageBtn");
+  const prevPageBtn = document.getElementById("prevPageBtn");
 const nextPageBtn = document.getElementById("nextPageBtn");
 const currentPageElement = document.getElementById("currentPage");
 const totalPagesElement = document.getElementById("totalPages"); // 新增获取总页数的元素
@@ -89,7 +94,7 @@ totalPagesElement.textContent = Math.ceil(filteredAnnouncements.length / itemsPe
 }
 
 function searchAnnouncements(keyword) {
-    const searchInput = document.getElementById("searchInput"); // 正确引用搜索输入框元素
+  const searchInput = document.getElementById("searchInput"); // 正确引用搜索输入框元素
 filteredAnnouncements = announcements.filter((announcement) => {
 return announcement.title.includes(keyword) || announcement.note.includes(keyword);
 });
@@ -110,36 +115,36 @@ updatePagination();
 
 // 事件处理
 document.getElementById("prevPageBtn").addEventListener("click", () => {
-  if (currentPage > 1) {
-    currentPage--;
-    updateAnnouncementTable();
-    updatePagination();
-  }
+if (currentPage > 1) {
+  currentPage--;
+  updateAnnouncementTable();
+  updatePagination();
+}
 });
 
 document.getElementById("nextPageBtn").addEventListener("click", () => {
-  if (currentPage * itemsPerPage < filteredAnnouncements.length) {
-    currentPage++;
-    updateAnnouncementTable();
-    updatePagination();
-  }
+if (currentPage * itemsPerPage < filteredAnnouncements.length) {
+  currentPage++;
+  updateAnnouncementTable();
+  updatePagination();
+}
 });
 
 document.getElementById("searchButton").addEventListener("click", () => {
-  const searchInput = document.getElementById("searchInput");
-  const keyword = searchInput.value.trim();
-  if (keyword) {
-    searchAnnouncements(keyword);
-  }
+const searchInput = document.getElementById("searchInput");
+const keyword = searchInput.value.trim();
+if (keyword) {
+  searchAnnouncements(keyword);
+}
 });
 
 // 在搜索框内输入时，显示所有公告
 document.getElementById("searchInput").addEventListener("input", () => {
-  if (!searchInput.value.trim()) {
-    filteredAnnouncements = announcements;
-    updateAnnouncementTable();
-    updatePagination();
-  }
+if (!searchInput.value.trim()) {
+  filteredAnnouncements = announcements;
+  updateAnnouncementTable();
+  updatePagination();
+}
 });
 
 
